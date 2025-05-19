7ad7e215c0ebeeeeadd4ccfef3756c60a4ae9e31
Abhi Udayshankar <abhi.udayshankar@kellogg.northwestern.edu>
2025-05-19 22:53:53 +0000

    completed Place task

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 # get("/X", {controller: => "X", :action => "X"})
 # get("/Y", {controller: => "Y", :action => "Y"})
-
+get("/", { :controller => "places", :action => "index"})
 resources "entries"
-resource "places"
+resources "places"
   # Defines the root path route ("/")
   # get("/", { :controller => "articles", :action => "index" })
 end
