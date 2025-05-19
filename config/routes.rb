5728ab5a43f17aec6ab032241fdb1a74baf39117
Abhi Udayshankar <abhi.udayshankar@kellogg.northwestern.edu>
2025-05-19 22:17:27 +0000

    Intitial Updates to controllers & Views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# get("/X", {controller: => "X", :action => "X"})
+# get("/Y", {controller: => "Y", :action => "Y"})
 
+resources "entries"
+resource "places"
   # Defines the root path route ("/")
   # get("/", { :controller => "articles", :action => "index" })
 end
